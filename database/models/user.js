aa0b6141d8fe60c6bf0cc7c76231ef3325458543
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 21:31:26 -0400

    Install crypto and write out User model with class and instance methods for encryption and password verification

---
+++ database/models/user.js
@@ -0,0 +1,54 @@
+const crypto = require("crypto");
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue("password");
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue("salt");
+    }
+  },
+  googleId: {
+    type: Sequelize.STRING
+  }
+});
+
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString("base64");
+};
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash("RSA-SHA256")
+    .update(plainText)
+    .update(salt)
+    .digest("hex");
+};
+
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
+
+const setSaltAndPassword = user => {
+  if (user.changed("password")) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
+};
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+
+module.exports = User;
