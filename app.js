96980fc39f61126ec3eba3cad1d6f64859fe6226
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 21:26:28 -0400

    Fix typo and install pg and sequelize

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 
-const SequelizeStore = require("connect-session-sequelize"(session.Store));
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./database");
 const sessionStore = new SequelizeStore({ db });
 
