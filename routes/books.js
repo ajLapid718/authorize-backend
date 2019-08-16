196786a7777309963be62d5325c5b47dafb8697b
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 22:12:01 -0400

    Finish up most of the RESTful API

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
   console.log("req.user is???", req.user)
-  res.status(200).json("HERE WOULD BE AN ARRAY OF BOOKS");
+  res.status(200).json([{id: 1, title: "Where The Red Fern Grows"}, {id: 2, title: "Cracking The Coding Interview"}]);
 });
 
 module.exports = router;
