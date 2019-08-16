62adb80b71d73ef446e7c6b32be699244a524a5f
ajLapid718 <ajLapid718@gmail.com>
2019-08-15 21:44:54 -0400

    Store user id with passport once and then deserialize the user and match with id thereafter

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ const db = require("./database");
 const sessionStore = new SequelizeStore({ db });
 
 const app = express();
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  }
+  catch (err) {
+    done(err);
+  }
+});
